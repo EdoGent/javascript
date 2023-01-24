b4cce9a97db1650b646d80c646659419fe826a96
Edoardo Gentile <edogen22122003@gmail.com>
2023-01-24 15:22:37 +0100

    object method exercise

---
+++ ObjectMethod.js
@@ -0,0 +1,11 @@
+const person = {
+    firstName: 'Mario',
+    lastName: 'Rossi',
+    age: 25
+}
+
+let keys = Object.keys(person);
+let values = Object.values(person);
+
+console.log(keys);
+console.log(values);
